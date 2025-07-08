86fa0927625a85b1e2247d7e06130d917a43a942
Luis Felipe Jr <felipequineper@hotmail.com>
2025-07-08 11:57:15 -0300

    adjusting deploy errors

--- src/app/login/user/page.tsx
+++ src/app/login/user/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+import { Suspense } from "react";
 import LoginForm from "../../components/LoginForm";
 
 export default function LoginUser() {
   return (
     <div className="min-h-screen flex items-center justify-center px-6 py-10 font-[family-name:var(--font-geist-sans)]">
-      <LoginForm />
+      <Suspense fallback={<a>loading</a>}>
+        
+        <LoginForm/>
+      </Suspense>
+
     </div>
   );
 }
