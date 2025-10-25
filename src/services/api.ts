aaf830abaa6eb0a4dbd4ac29febb9b8b10ff63ed
Luis Felipe Jr <felipequineper@hotmail.com>
2025-10-25 04:08:03 -0300

    feat: adjusting api service credentials

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,11 +8,9 @@ const { 'easygas.token': token } = parseCookies();
 export const api = axios.create({
   // Use uma variável de ambiente para a URL da sua API
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333',
+  withCredentials: true
 });
 
-// Adiciona o token ao cabeçalho de autorização se ele existir
-// Isso garante que as chamadas feitas logo após o carregamento da página
-// (no lado do cliente) já estejam autenticadas.
 if (token) {
   api.defaults.headers['Authorization'] = `Bearer ${token}`;
 }
